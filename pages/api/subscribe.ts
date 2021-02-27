47c355252e25555a06ab61be96ac7d6a164b90c0
murilokrugner <murilokrugner@hotmail.com>
2021-02-27 15:13:40 -0300

    first commit

---
+++ pages/api/subscribe.ts
@@ -0,0 +1,40 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+import { MongoClient, Db } from 'mongodb';
+
+import url from 'url';
+
+let cacheDb: Db = null;
+
+async function connectToDatabase(uri: string) {
+    if (cacheDb) {
+        return cacheDb;
+    }
+
+    const client = await MongoClient.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    const dbName = url.parse(uri).pathname.substr(1);
+
+    const db = client.db(dbName);
+
+    cacheDb = db;
+
+    return db;
+}
+
+export default async (request: NowRequest, response: NowResponse) => {
+    const { email } = request.body;
+
+    const db = await connectToDatabase(process.env.MONGODB_URI);
+
+    const collection = db.collection('subscribers');
+
+    await collection.insertOne({
+        email,
+        subscribeAt: new Date(),
+    });
+
+    return response.status(201).json({ ok: true });
+}
